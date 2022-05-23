7ab91123028974a461e9a6673c2bfb05ec44c1ff
Md Tariqul Islam <programmertariq@gmail.com>
2022-05-23 18:16:41 +0600

    first connection with mongodb server

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("express-async-errors");
+
+// const rootRouter = require("./routes/root");
+// const tasksRouter = require("./routes/tasks");
+// const loginRouter = require("./routes/login");
+
+// // import middlewares
+// const errorsMiddleware = require("./middlewares/errors");
+
+const app = express();
+if (app.get("env") !== "production") {
+  require("dotenv").config();
+}
+
+// Connecting to database
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1oxkl.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+const db = mongoose.connection;
+db.on("error", (err) => console.error(err));
+db.once("open", () => console.log("connected to mongoose"));
+
+///
+
+///
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`server is listening on port ${port}`));
