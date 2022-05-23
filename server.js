7e45ace31dae2798a3b8228bc168c497d48f4f49
Md Tariqul Islam <programmertariq@gmail.com>
2022-05-23 18:22:28 +0600

    root route for testing the server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("express-async-errors");
 
-// const rootRouter = require("./routes/root");
+const rootRouter = require("./routes/root");
 // const tasksRouter = require("./routes/tasks");
 // const loginRouter = require("./routes/login");
 
@@ -26,9 +26,13 @@ const db = mongoose.connection;
 db.on("error", (err) => console.error(err));
 db.once("open", () => console.log("connected to mongoose"));
 
-///
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+// routes
+app.use("/", rootRouter);
+// app.use("/login", loginRouter);
 app.use(errorsMiddleware);
-///
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`server is listening on port ${port}`));
